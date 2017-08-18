18fe7a2fcb1afcc7d210c867b5665a092b492f6a
cordwisteria <cordwisteria@gmail.com>
2017-08-18 13:40:14 +0000

    Fix

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -2,4 +2,4 @@
 ln -sf ~/dotfiles/keybind.zsh ~/.zsh.after/keybind.zsh
 ln -sf ~/dotfiles/prompt.zsh ~/.zsh.after/prompt.zsh
 ln -sf ~/dotfiles/.tmux.conf.user ~/.tmux.conf.user
-
+ln -sf ~/dotfiles/.vimrc.after ~/.vimrc.after
