689d71476d6275077b05f1c699d10e5a70e144fa
綱藤公一郎 <menbou@menbou-Mac-mini.local>
2023-02-11 18:24:49 +0900

    Remove brew.zsh

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 ln -sf ~/dotfiles/keybind.zsh ~/.zsh.after/keybind.zsh
 ln -sf ~/dotfiles/prompt.zsh ~/.zsh.after/prompt.zsh
-ln -sf ~/dotfiles/brew.zsh ~/.zsh.after/brew.zsh
 ln -sf ~/dotfiles/.tmux.conf.user ~/.tmux.conf.user
 ln -sf ~/dotfiles/.vimrc.before ~/.vimrc.before
